facfb3deae6d159f1e04e085e3142a15f5eda978
THEEALPHA254 <gracemuriithi2004@gmail.com>
2023-09-23 15:25:07 +0300

    first OOP1 project

--- cylinderV.py
+++ cylinderV.py
@@ -1,14 +1,15 @@
-#calculate the volume of a cylinder 
+#impotr math package to use math.pi for the value of PI 
 #volume = pi*(r)^2*h
+import math
 def cylinder_volume():
-    return (pi*(square(r))*h)
+    return (math.pi*(pow(r,2))*h)
 
 def square(x):
     return x*x 
 
 r = int (input("enter radius: "))
 h = int (input("enter height: "))
-pi= 22/7
+#calculate the volume of a cylinder 
 v = cylinder_volume()
 
-print(v)
+print("The volume of the cylinder is: ", v)
